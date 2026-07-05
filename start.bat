e7a5df342486b3781509f2435faef791fcdd9a01
Hansjoerg Hofer <hansjoerghofer692@gmail.com>
2026-07-05 13:14:13 +0200

    chore: sync project changes and setup memory configuration

--- start.bat
+++ start.bat
@@ -1,5 +1,8 @@
 @echo off
-cd /d C:\dev\HermesClawZero-ConfigSidecar
+cd /d "%~dp0"
+
+echo [START] Cleaning old watchdog processes...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-CimInstance Win32_Process | Where-Object { $_.Name -match 'python' -and $_.CommandLine -match 'sync_watchdog.py' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force -ErrorAction SilentlyContinue }"
 
 echo [START] System Services...
 start "" python sync_watchdog.py
